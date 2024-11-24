60c0f6b3573472592cb3cc89b8c9ef1487222414
mdarf <mdarfinji45@gmail.com>
2024-11-24 19:22:35 +0800

    sendwelcome email added

---
+++ mailtrap/email.ts
@@ -0,0 +1,47 @@
+import {
+  generatePasswordResetEmailHtml,
+  generateResetSuccessEmailHtml,
+  generateWelcomeEmailHtml,
+  htmlContent,
+} from "./htmlEmail";
+
+import { client, sender } from "./mailtrap";
+
+export const sendVerificationEmail = async (
+  email: string,
+  verificationToken: string
+) => {
+  const recipient = [{ email }];
+  try {
+    const res = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify your email",
+      html: htmlContent.replace("{verificationToken}", verificationToken),
+      category: "Email Verification",
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send email verification");
+  }
+};
+
+export const sendWelcomeEmail = async (email: string, name: string) => {
+  const recipient = [{ email }];
+  const htmlContent = generateWelcomeEmailHtml(name);
+  try {
+    const res = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Welcome to FoodHub",
+      html: htmlContent,
+      template_variables: {
+        company_info_name: "FoodHub",
+        name: name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send welcome email");
+  }
+};
