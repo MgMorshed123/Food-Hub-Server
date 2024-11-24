12f980b6158b1eb99cb319aa094addaf0ad23fe8
mdarf <mdarfinji45@gmail.com>
2024-11-24 19:52:18 +0800

    user route done

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB";
-
+import userRoutes from "./routes/user.route";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//
+app.use("/api/v1/user", userRoutes);
 
 app.listen(PORT, () => {
   connectDB();
