861e8481eacce5688999635f0c6989887abb8f32
mdarf <mdarfinji45@gmail.com>
2024-11-24 20:27:58 +0800

    user route done

--- index.ts
+++ index.ts
@@ -2,11 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB";
 import userRoutes from "./routes/user.route";
+import bodyParser from "body-parser";
 dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: "https://food-app-yt.onrender.com",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 //
 app.use("/api/v1/user", userRoutes);
 
