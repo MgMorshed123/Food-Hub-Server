be5ea9b1f8157367732f65c5ba0f07dd6dd5fe3b
mdarf <mdarfinji45@gmail.com>
2024-11-24 14:47:29 +0800

    database added

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB";
 
 dotenv.config();
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server listening at port ${PORT}`);
 });
