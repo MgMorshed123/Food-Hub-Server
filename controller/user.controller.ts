d3de24a2a2698be8a9c0d57ca57705b361948b66
mdarf <mdarfinji45@gmail.com>
2024-11-24 17:09:22 +0800

    user password chcekced

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -55,6 +55,15 @@ export const login = async (req: Request, res: Response) => {
         message: "Incorrect email or Password",
       });
     }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
   } catch (error) {
     return res.status(500).json({ message: "Interna server error" });
   }
