420a06e362d0b672522a002606033660966565be
mdarf <mdarfinji45@gmail.com>
2024-11-24 19:25:40 +0800

    sendResetSuccessEmail done

--- mailtrap/email.ts
+++ mailtrap/email.ts
@@ -45,3 +45,40 @@ export const sendWelcomeEmail = async (email: string, name: string) => {
     throw new Error("Failed to send welcome email");
   }
 };
+
+export const sendPasswordResetEmail = async (
+  email: string,
+  resetURL: string
+) => {
+  const recipient = [{ email }];
+  const htmlContent = generatePasswordResetEmailHtml(resetURL);
+  try {
+    const res = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: htmlContent,
+      category: "Reset Password",
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to reset password");
+  }
+};
+
+export const sendResetSuccessEmail = async (email: string) => {
+  const recipient = [{ email }];
+  const htmlContent = generateResetSuccessEmailHtml();
+  try {
+    const res = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Password Reset Successfully",
+      html: htmlContent,
+      category: "Password Reset",
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send password reset success email");
+  }
+};
