f3cbb325532945c005e7cdabce6ff8b58f16f33f
mdarf <mdarfinji45@gmail.com>
2024-11-26 03:04:35 +0800

    added enu model

---
+++ controller/menu.controller.ts
@@ -0,0 +1,33 @@
+export const addMenu = async (req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body;
+    const file = req.file;
+    if (!file) {
+      return res.status(400).json({
+        success: false,
+        message: "Image is required",
+      });
+    }
+    const imageUrl = await uploadImageOnCloudinary(file as Express.Multer.File);
+    const menu: any = await Menu.create({
+      name,
+      description,
+      price,
+      image: imageUrl,
+    });
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (restaurant) {
+      (restaurant.menus as mongoose.Schema.Types.ObjectId[]).push(menu._id);
+      await restaurant.save();
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: "Menu added successfully",
+      menu,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
